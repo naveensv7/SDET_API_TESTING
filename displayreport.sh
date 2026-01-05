ccd6cdfb39d7abd1251c5a708501309881cdea97
Naveen S V <naveensv77@gmail.com>
2026-01-06 02:38:22 +0530

    Resolved issues and tried

--- displayreport.sh
+++ displayreport.sh
@@ -2,5 +2,5 @@
 set -e
 
 echo "Generate the report from results"
-npx allure generate allure-results --clean -o allure-report
+npx allure generate /allure/allure-results --clean -o /allure/allure-report
 
